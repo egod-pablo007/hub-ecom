11607583734d94d58bc0b11a9e94acae918d92af
egod-pablo007 <egod.pablo@gmail.com>
2025-08-04 19:09:03 -0700

    09-create-product-details-page

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,3 +4,5 @@ export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Hub Ecommerce'
 
 export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'Hub Ecommerce'
+
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
