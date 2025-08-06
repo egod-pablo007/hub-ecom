e051bc5f0cbff80fa4a6e8b11b2b5ac484492849
egod-pablo007 <egod.pablo@gmail.com>
2025-08-05 22:02:59 -0700

    11-implement-add-to-cart

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,5 +1,6 @@
 import { ShoppingCartIcon } from 'lucide-react'
 import Link from 'next/link'
+import CartButton from './cart-button'
 
 export default function Menu() {
   return (
@@ -8,10 +9,7 @@ export default function Menu() {
         <Link href="/signin" className="flex items-center header-button">
           Hello, Sign In
         </Link>
-        <Link href="/cart" className="header-button">
-          <ShoppingCartIcon className="h-8 w-8" />
-          <span className="font-bold">Cart</span>
-        </Link>
+        <CartButton />
       </nav>
     </div>
   )
--- lib/constants.ts
+++ lib/constants.ts
@@ -6,3 +6,7 @@ export const APP_DESCRIPTION =
 export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'Hub Ecommerce'
 
 export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
+
+export const FREE_SHIPPING_MIN_PRICE = Number(
+  process.env.FREE_SHIPPING_MIN_PRICE || 35
+)
