a5b844f24c5c2287c2fd1ad84b02292e1a499608
egod-pablo007 <egod.pablo@gmail.com>
2025-08-16 23:39:01 -0700

    14-signin-user

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,6 +4,9 @@ export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Hub Ecommerce'
 
 export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'Hub Ecommerce'
+export const APP_COPYRIGHT =
+  process.env.NEXT_PUBLIC_APP_COPYRIGHT ||
+  `Copyright © 2025 ${APP_NAME}. All rights reserved.`
 
 export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
 
